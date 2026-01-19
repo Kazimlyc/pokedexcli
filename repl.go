97d7656e67ddc388479d95c148cc91b1b3358295
Kazimlyc <kazimkalayc@hotmail.com>
2026-01-20 02:14:11 +0300

    Improve cleanInput function and expand tests
    Refactored cleanInput to use strings.Fields and normalize input by trimming spaces and converting to lowercase. Added multiple test cases to repl_test.go to verify correct behavior for various input scenarios.

--- repl.go
+++ repl.go
@@ -1,6 +1,15 @@
 package main
 
+import "strings"
+
 func cleanInput(text string) []string {
 	result := []string{}
+	arr := strings.Fields(text)
+	for _, i := range arr {
+		a := strings.TrimSpace(i)
+		a = strings.ToLower(a)
+		result = append(result, a)
+	}
+
 	return result
 }
--- repl_test.go
+++ repl_test.go
@@ -1,7 +1,44 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestCleanInput(t *testing.T) {
+
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		// add more cases here
+		{
+			input:    " selam dunya",
+			expected: []string{"selam", "dunya"},
+		}, {
+			input:    " selam dunya naber",
+			expected: []string{"selam", "dunya", "naber"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		if len(actual) != len(c.expected) {
+			t.Errorf("fail")
+			continue
+		}
+
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord {
+				t.Errorf("fail")
+				break
+			}
+		}
+	}
 
 }
