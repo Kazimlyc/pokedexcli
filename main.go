32e519ea4e6d89e6e981b10f9520ef3c1ccb4ebc
Kazimlyc <kazimkalayc@hotmail.com>
2026-01-21 03:43:57 +0300

    Add CLI command handling with help and exit commands
    Introduced a cliCommand struct and a command registry to handle user commands. Implemented 'help' and 'exit' commands with descriptions and callbacks, improving the CLI structure and user experience.

--- main.go
+++ main.go
@@ -7,7 +7,14 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
 func main() {
+
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
@@ -15,6 +22,44 @@ func main() {
 		input := scanner.Text()
 		input = strings.Fields(input)[0]
 		input = strings.ToLower(input)
-		fmt.Println("Your command was:", input)
+		command, exists := getCommands()[input]
+		if !exists {
+			fmt.Println("Unknown command")
+			continue
+		}
+		if err := command.callback(); err != nil {
+			fmt.Println(err)
+		}
+	}
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+	for _, cmd := range getCommands() {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	return nil
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
 	}
 }
