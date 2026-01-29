82ae9fa5f84749bcee5311fdca952921bd6cb24c
Kazimlyc <kazimkalayc@hotmail.com>
2026-01-29 14:57:29 +0300

    Refactor pokecache to new package structure
    Moved pokecache implementation from internal/pokecache.go to internal/pokecache/pokecache.go. Introduced a mutex and map for cache storage, and stubbed out Add and Get methods for future implementation.

---
+++ internal/pokecache/pokecache.go
@@ -0,0 +1,28 @@
+package pokecache
+
+import (
+	"sync"
+	"time"
+)
+
+type Cache struct {
+	mu    sync.Mutex
+	cache map[string]cacheEntry
+}
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+func NewCache(interval time.Duration) Cache {
+
+	return Cache{}
+}
+
+func (c *Cache) Add(key string, val []byte) {
+
+}
+
+func (c *Cache) Get(key string, val []byte) {
+
+}
