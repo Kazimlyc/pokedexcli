eaa3332a096165954e5f807c9f2f5d9616029fd6
Kazimlyc <kazimkalayc@hotmail.com>
2026-01-30 18:06:23 +0300

    Implement cache add/get and tests for pokecache
    Fixed locking in Add and Get methods, implemented value retrieval in Get, and started the reap loop in NewCache. Added unit tests for Add, Get, and reapLoop behaviors in pokecache_test.go.

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -15,22 +15,32 @@ type cacheEntry struct {
 }
 
 func NewCache(interval time.Duration) Cache {
-
-	return Cache{}
+	c := Cache{
+		cache: make(map[string]cacheEntry),
+	}
+	go c.reapLoop(interval)
+	return c
 }
 
 func (c *Cache) Add(key string, val []byte) {
 	c.mu.Lock()
-	defer c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.cache[key] = cacheEntry{createdAt: time.Now(), val: val}
+
 }
 
-func (c *Cache) Get(key string, val []byte) {
+func (c *Cache) Get(key string) ([]byte, bool) {
 	c.mu.Lock()
-	defer c.mu.Lock()
+	defer c.mu.Unlock()
+	entry, ok := c.cache[key]
+	if !ok {
+		return nil, false
+	}
+	return entry.val, true
 }
 
 func (c *Cache) reapLoop(interval time.Duration) {
-
 	ticker := time.NewTicker(interval)
 	defer ticker.Stop()
 	for range ticker.C {
