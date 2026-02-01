36342adbb4076b9775178ba0ea3781ca69f83614
Kazimlyc <kazimkalayc@hotmail.com>
2026-02-01 19:52:08 +0300

    Add argument validation to commandExplore
    The commandExplore function now checks that exactly one argument (location name) is provided and returns an error if not. This prepares the function for further implementation using the provided area name.

--- command_explore.go
+++ command_explore.go
@@ -1,7 +1,17 @@
 package main
 
+import "fmt"
+
 // todo
 func commandExplore(cfg *config, args ...string) error {
+	if len(args) != 1 {
+		return fmt.Errorf("you must provide a location name")
+	}
+	areaName := args[0]
+
+	// TODO: use cfg.pokeapiClient.GetLocation(areaName) and print the results
+
+	_ = areaName
 
 	return nil
 }
